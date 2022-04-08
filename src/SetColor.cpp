f6fb4806e04d5857596bba51b3b9f4809f3d3d22
forgottenecho <matv201@gmail.com>
2022-04-08 14:26:53 -0400

    SetColor debug info

--- src/SetColor.cpp
+++ src/SetColor.cpp
@@ -2,6 +2,9 @@
 #include "SetColor.cpp"
 
 void colorInit(){
+  // begin serial for debugging
+  Serial.begin(9600);
+
   // initialize the master decorder
   pinMode(MASTER_EN, OUTPUT);
   pinMode(MASTER_S2, OUTPUT);
@@ -20,13 +23,16 @@ void colorInit(){
 }
 
 void ledOn(int moduleNum, int drawerNum){
-    
     // select proper drawer (LED) by updating slave's selects
+    Serial.print("Setting slave selects to ");
+    Serial.println(drawerNum);
     digitalWrite(SLAVE_S2, bitRead(drawerNum,2));
     digitalWrite(SLAVE_S1, bitRead(drawerNum,1));
     digitalWrite(SLAVE_S0, bitRead(drawerNum,0));
 
     // select proper slave decoder
+    Serial.print("Setting master selects to ");
+    Serial.println(moduleNum);
     digitalWrite(MASTER_S2, bitRead(moduleNum,2));
     digitalWrite(MASTER_S1, bitRead(moduleNum,1));
     digitalWrite(MASTER_S0, bitRead(moduleNum,0));
@@ -38,20 +44,24 @@ void ledOn(int moduleNum, int drawerNum){
 
 void ledOff(int moduleNum, int ledNum){
     // turn of master decoder, which turns off all LEDs
+    Serial.println("Turn all LED off");
     digitalWrite(MASTER_EN, LOW);
 }
 
 void setGreen(int moduleNum, int drawerNum){
+    Serial.println("Setting color green");
     analogWrite(R_CTRL, 255); // R is fully off
     analogWrite(G_CTRL, 0); // G is fully on
     analogWrite(B_CTRL, 255); // B is fully off
 }
 void setYellow(int moduleNum, int drawerNum){
+    Serial.println("Setting color yellow");
     analogWrite(R_CTRL, 0); // R is fully off
     analogWrite(G_CTRL, 0); // G is fully on
     analogWrite(B_CTRL, 255); // B is fully off
 }
 void setRed(int moduleNum, int drawerNum){
+    Serial.println("Setting color red");
     analogWrite(R_CTRL, 0); // R is fully on
     analogWrite(G_CTRL, 255); // G is fully off
     analogWrite(B_CTRL, 255); // B is fully off
